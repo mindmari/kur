b784ce32990021bc62e9aacc341d213c3e0ae656
mindmari <51208012+mindmari@users.noreply.github.com>
2019-12-13 20:52:21 +0300

    Настроенная среда для разработки

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Вы авторизованы!
                 </div>
             </div>
         </div>
